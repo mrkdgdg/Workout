2774483ca225515ac681a6130880357e8ca5b394
mrkdgdg <dagdag.markexequiel@gmail.com>
2025-11-22 00:19:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('workout-cache').then(cache => {
-      return cache.addAll(['/', '/index.html']);
+      return cache.addAll(['./', './index.html']);
     })
   );
 });
